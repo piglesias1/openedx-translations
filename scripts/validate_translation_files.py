f2274f54cfa94316914c5583cc28cdf141e723fe
Pablo Iglesias <39501872+piglesias1@users.noreply.github.com>
2025-03-26 23:10:03 +0000

    Add some functionalities to validation script

--- scripts/validate_translation_files.py
+++ scripts/validate_translation_files.py
@@ -11,6 +11,7 @@ import textwrap
 
 import i18n.validate
 
+
 def get_translation_files(translation_directory):
     """
     List all translations '*.po' files in the specified directory.
@@ -68,11 +69,14 @@ def validate_translation_file(po_file):
     return {
         'valid': valid,
         'output': output,
+        'failures': len(problems),
     }
 
 
 def validate_translation_files(
     translations_dir='translations',
+    all_files=False,
+    full_report=False,
 ):
     """
     Run GNU gettext `msgfmt` and print errors to stderr.
@@ -83,20 +87,27 @@ def validate_translation_files(
       return 1 for invalid translations.
     """
     translations_valid = True
-
     invalid_lines = []
+    total_failures = 0
 
     po_files = get_translation_files(translations_dir)
+
     for po_file in po_files:
         result = validate_translation_file(po_file)
 
-        if result['valid']:
-            print('VALID: ' + po_file)
-            print(result['output'], '\n' * 2)
-        else:
-            invalid_lines.append('INVALID: ' + po_file)
-            invalid_lines.append(result['output'] + '\n' * 2)
+        if not result['valid']:
+            invalid_lines.append(f"INVALID: {po_file} | Failures: {result['failures']}")
+            total_failures += result['failures']
+
+            if full_report:
+                invalid_lines.append(result['output'] + '\n' * 2)
+
             translations_valid = False
+        elif all_files:
+            print('VALID: ' + po_file)
+
+            if full_report:
+                print(result['output'], '\n' * 2)
 
     # Print validation errors in the bottom for easy reading
     print('\n'.join(invalid_lines), file=sys.stderr)
@@ -107,9 +118,9 @@ def validate_translation_files(
         print('-----------------------------------------')
         exit_code = 0
     else:
-        print('---------------------------------------', file=sys.stderr)
-        print('FAILURE: Some translations are invalid.', file=sys.stderr)
-        print('---------------------------------------', file=sys.stderr)
+        print('------------------------------------------', file=sys.stderr)
+        print(f'FAILURE: {total_failures} translations are invalid.', file=sys.stderr)
+        print('------------------------------------------', file=sys.stderr)
         exit_code = 1
 
     return exit_code
@@ -117,10 +128,32 @@ def validate_translation_files(
 
 def main():  # pragma: no cover
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('--dir', action='store', type=str, default='translations')
+    parser.add_argument('-f', '--filename', action='store', type=str, default=None, help='Filename to validate')
+    parser.add_argument('-d','--dir', action='store', type=str, default='translations', help='Translations directory')
+    parser.add_argument('-F', '--full-report', action='store_true', help='Print full output')
+    parser.add_argument('-a', '--all', action='store_true', help='Include valid files in output')
+
     args = parser.parse_args()
+
+    if args.filename:
+        if not os.path.exists(args.filename):
+            print(f"ERROR: {args.filename} does not exist.")
+            sys.exit(1)
+
+        result = validate_translation_file(args.filename)
+
+        if not result['valid']:
+            print(f"INVALID: {args.filename} | Failures: {result['failures']}")
+            print(result['output'] + '\n' * 2)
+            sys.exit(1)
+
+        sys.exit(0)
+
+
     sys.exit(validate_translation_files(
         translations_dir=args.dir,
+        all_files=args.all,
+        full_report=args.full_report,
     ))
 
 
